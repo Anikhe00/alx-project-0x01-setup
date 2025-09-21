10ce8cd99282934ced026c1457888befe67a9a96
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-21 20:42:23 +0100

    Update the Users Component for the checker

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,8 +2,8 @@ import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
-  console.log(users);
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -15,8 +15,8 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2">
-          {users?.map((user) => (
-            <UserCard key={user.id} {...user} />
+          {posts?.map((post) => (
+            <UserCard key={post.id} {...post} />
           ))}
         </div>
       </main>
@@ -26,11 +26,11 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json();
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
