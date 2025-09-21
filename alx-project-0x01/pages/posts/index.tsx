bb4f8a06aba5f1efbf5dee56565099d7ca0b7c23
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-21 21:29:18 +0100

    Extend Post Page

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,20 +1,33 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
 const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
-  console.log(posts);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [allPosts, setAllPosts] = useState<PostData[]>([]);
+
+  const handleAddPost = (newPost: PostData) => {
+    const postWithID = { ...newPost, id: allPosts.length + 1 };
+    setAllPosts([postWithID, ...allPosts]);
+    setModalOpen(false);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
             Add Post
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-2">
+        <div className="grid grid-cols-3 gap-2 ">
           {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
             <PostCard
               title={title}
@@ -26,6 +39,17 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
           ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
+            <PostModal
+              onClose={() => setModalOpen(false)}
+              onSubmit={handleAddPost}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
@@ -33,6 +57,7 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await response.json();
+
   return {
     props: {
       posts,
